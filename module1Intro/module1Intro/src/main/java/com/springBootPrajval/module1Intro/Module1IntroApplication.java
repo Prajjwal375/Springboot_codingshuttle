f94643b7007a93efe6f1ef4161c40912957de6f5
PrajvalSaki <166584486+Prajjwal375@users.noreply.github.com>
2026-06-22 14:41:56 +0530

    coding shutle course

---
+++ module1Intro/module1Intro/src/main/java/com/springBootPrajval/module1Intro/Module1IntroApplication.java
@@ -0,0 +1,13 @@
+package com.springBootPrajval.module1Intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1IntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module1IntroApplication.class, args);
+	}
+
+}
---
+++ module1Intro/module1Intro/src/test/java/com/springBootPrajval/module1Intro/Module1IntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootPrajval.module1Intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1IntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
